290d1c635965de384dfa393206c7395733299d45
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2021-03-14 20:43:40 +0100

    Initial commit

---
+++ src/main/java/pl/jakubkowalik/springpastebin/SpringPastebinApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubkowalik.springpastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPastebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPastebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jakubkowalik/springpastebin/SpringPastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubkowalik.springpastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPastebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
